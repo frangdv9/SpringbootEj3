b4a802d1543839bc45642b4f17584eb7133d43e1
Fran <frangomez0409@gmail.com>
2024-05-29 21:58:10 -0300

    first commit

---
+++ src/main/java/com/example/springbootej3/SpringbootEj3Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootej3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEj3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEj3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootej3/SpringbootEj3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootej3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEj3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
